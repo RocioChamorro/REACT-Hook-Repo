c090440d2969e1711ef998384627d47128c5b421
Rocio del Pilar Chamorro Ramos <rocio_chamorro@outlook.com>
2022-04-30 09:36:57 -0500

    hook repository

---
+++ useFetch/useFetch.js
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from "react";
+
+const useFetch = (url) => {
+  const isMounted = useRef(true);
+  const [state, setState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    console.log("nieto")
+    return () => {
+      //aquí se ejecuta cuando el componente se desmonta
+      isMounted.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    setState({
+      data: null,
+      loading: true,
+      error: null,
+    });
+
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (isMounted.current) {
+          setState({
+            loading: false,
+            error: null,
+            data,
+          });
+        } else {
+          console.log("setState no se llamó")
+        }
+      });
+  }, [url]);
+  return state;
+};
+
+export default useFetch;
